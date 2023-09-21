41a4e35d9659f1c31668e72ab40d7d7a68383779
skjeong <skjeong@contec.kr>
2023-09-21 10:45:13 +0900

    Initial commit

---
+++ src/main/java/kr/re/contec/splitremostelemetry/SplitRemosTelemetryApplication.java
@@ -0,0 +1,13 @@
+package kr.re.contec.splitremostelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitRemosTelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitRemosTelemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/re/contec/splitremostelemetry/SplitRemosTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.re.contec.splitremostelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitRemosTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
